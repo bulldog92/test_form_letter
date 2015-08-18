39532210b1b9f241aad6af931429b48a330a8ab7
bulldog92 <dmytro_kozynets@mail.ru>
2015-08-18 21:20:15 +0000

    third

--- js/scroll.js
+++ js/scroll.js
@@ -1,18 +1,21 @@
  (function(){
-        var $letterElem  = $('.form-wrapper');
-        var $containerHeight = $letterElem.outerHeight();
-        console.log($containerHeight);
-        var $topElem;
-        changePos();
-        $(window).scroll(function() {
-            changePos();
-
-            
-        });
-        function changePos(){
-                            if($(window).scrollTop() <= $containerHeight && $(window).scrollTop() <= '240' && $(window).scrollTop() >= '0' ){
-                $topElem  = $(window).scrollTop();
-                $($letterElem).animate({'bottom': -$topElem}, 10);
-                            };
+        $("#show-letter").on('click', function() {
+                    showLetter()
+                return false;
+                });
+        $('#hide-letter').on('click', function() {
+                    hideLetter();
+                    return false;
+                })
+        function showLetter(){
+            $('.container').css({'display':'block'}).animate({'height' : '650px'},2000); 
+                    $('.form-wrapper').animate({'bottom' : '0px'}, 3000);
         };
+        function hideLetter() {
+                    $('.form-wrapper').animate({'bottom' : '-480px'}, 1500, function(){
+                        $('.container').animate({'height' : '0'},2000);    
+                    });
+                     
+        };
+    
 }) ();
