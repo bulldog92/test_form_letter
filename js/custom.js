dd488cb74a411dd8bdac7a76685bad37269eace8
bulldog92 <dmytro_kozynets@mail.ru>
2015-08-18 21:50:39 +0000

    script_done

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
  (function(){
         $("#show-letter").on('click', function() {
-                    showLetter()
+                    showLetter();
                 return false;
                 });
         $('#hide-letter').on('click', function() {
@@ -9,13 +9,15 @@
                 })
         function showLetter(){
             $('.container').css({'display':'block'}).animate({'height' : '650px'},2000); 
-                    $('.form-wrapper').animate({'bottom' : '0px'}, 3000);
+                    $('.form-wrapper').animate({'bottom' : '0px'}, 2500);
         };
         function hideLetter() {
-                    $('.form-wrapper').animate({'bottom' : '-480px'}, 1500, function(){
-                        $('.container').animate({'height' : '0'},2000);    
-                    });
-                     
+                    $('.form-wrapper').animate({'bottom' : '-500px'}, 1500);
+                        $('.container').animate({'height' : '0'},2000);
         };
+        $(".nav-gorizontal a").on("click", function(){
+            $(".nav-gorizontal a").removeClass("active");
+            $(this).addClass("active");
+        });
     
 }) ();
