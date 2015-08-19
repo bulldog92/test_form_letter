673d8857cd94f552c490d29a7d1d4fd44b2b9f35
bulldog92 <dmytro_kozynets@mail.ru>
2015-08-19 18:40:00 +0000

    form

--- js/custom.js
+++ js/custom.js
@@ -8,12 +8,19 @@
                     return false;
                 })
         function showLetter(){
+             if($(window).scrollTop() != '0'){
+                           $('body').animate({'scrollTop':'0'},400);; 
+                        };
             $('.container').css({'display':'block'}).animate({'height' : '650px'},2000); 
                     $('.form-wrapper').animate({'bottom' : '0px'}, 2500);
         };
         function hideLetter() {
+             if($(window).scrollTop() != '0'){
+                           $('body').animate({'scrollTop':'0'},400);; 
+                        };
                     $('.form-wrapper').animate({'bottom' : '-500px'}, 1500);
                         $('.container').animate({'height' : '0'},2000);
+                       
         };
         $(".nav-gorizontal a").on("click", function(){
             $(".nav-gorizontal a").removeClass("active");
